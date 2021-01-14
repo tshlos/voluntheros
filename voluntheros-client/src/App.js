67ccf2e86957b8ab9a8adb66e463ff50ca2a103e
cortezd334 <cortezd334@gmail.com>
2021-01-14 14:52:42 -0800

    diff

--- voluntheros-client/src/App.js
+++ voluntheros-client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { Component } from 'react';
 import TaskList from './components/TaskList';
 import Login from './components/Login'
+import HomePage from './components/HomePage'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <h1> HELLO </h1>
       <TaskList />
       <Login />
+      <HomePage/>
     </div>
   );
 }
