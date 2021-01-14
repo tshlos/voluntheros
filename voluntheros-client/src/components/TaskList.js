0342df90864e4a049fc0128df1119fed2d387e19
Taci Shlosberg <tshlosberg@gmail.com>
2021-01-14 14:05:42 -0800

    Create tasklist

---
+++ voluntheros-client/src/components/TaskList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class TaskList extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <h1> TASK LIST </h1>
+               
+            </div>
+        )
+    }
+}
