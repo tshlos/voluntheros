60876832140efe47bc5f35be08b6533ede95393d
cortezd334 <cortezd334@gmail.com>
2021-01-14 16:42:35 -0800

    controllers

--- voluntheros-server/app/controllers/elderlies_controller.rb
+++ voluntheros-server/app/controllers/elderlies_controller.rb
@@ -1,2 +1,35 @@
 class ElderliesController < ApplicationController
+
+    def index
+        elderlies = Elderly.all
+        render json: elderlies
+    end
+
+    def show
+        elderly = Elderly.find(params[:id])
+        render json: elderly
+    end
+
+    def create
+        elderly = Elderly.create(elderly_params)
+        render json: elderly
+    end
+
+    def update
+        elderly = Elderly.find(params[:id])
+        elderly.update(elderly_params)
+        render json: elderly
+    end
+
+    def destroy
+        elderly = Elderly.find(params[:id])
+        elderly.destroy
+        render json: {error: "Your account has been deleted"}
+    end
+
+    private
+    def elderly_params
+        params.require(:elderly).permit(:name, :username, :age, :phone, :disability, :housing)
+    end
+    
 end
--- voluntheros-server/app/controllers/volunteers_controller.rb
+++ voluntheros-server/app/controllers/volunteers_controller.rb
@@ -1,2 +1,35 @@
 class VolunteersController < ApplicationController
+
+    def index
+        volunteers = Volunteer.all
+        render json: volunteers
+    end
+
+    def show
+        volunteer = Volunteer.find(params[:id])
+        render json: volunteer
+    end
+
+    def create
+        volunteer = Volunteer.create(volunteer_params)
+        render json: volunteer
+    end
+
+    def update
+        volunteer = Volunteer.find(params[:id])
+        volunteer.update(volunteer_params)
+        render json: volunteer
+    end
+
+    def destroy
+        volunteer = Volunteer.find(params[:id])
+        volunteer.destroy
+        render json: {error: "Your account has been deleted"}
+    end
+
+    private
+    def volunteer_params
+        params.require(:volunteer).permit(:name, :username, :age, :phone)
+    end
+    
 end
