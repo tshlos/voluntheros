ea45e44dce9b9c91875a712e00eae4c522ce53ea
keeks5456 <Akikogreen08@gmail.com>
2021-01-15 13:10:03 -0800

    elders profile working

--- voluntheros-client/src/components/Elders.js
+++ voluntheros-client/src/components/Elders.js
@@ -1,14 +1,42 @@
 import React, { useEffect } from 'react';
 import TaskList from './TaskList';
 // import { createTasks } from './api';
+import { Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
-function Elders({task}) {
 
+
+function Elders({ task }) {
+    // username
+    const tapioca = 'rubyred'
+
+    console.log(task, 'task?')
+    function displayTasks() {
+        const mayTask = task.filter(task => task.elderly_id === 1)
+        console.log(mayTask)
+        return mayTask.map(task => {
+            return <>
+                <Card className='grid' key={task.id} style={{ width: '18rem' }}>
+                    <Card.Header>{task.title}</Card.Header>
+                    <Card.Text>
+                        Details: {task.details === null ? 'None' : task.details}
+                        {/* {task.date} */}
+                    </Card.Text>
+                    {task.elderly_id === null ?
+                        <Button>Add to My Tasks</Button>
+                        :
+                        <Button disabled>No Longer Available</Button>
+                    }
+                </Card>
+            </>
+
+        })
+    }
     return (
-        <div>
-            <h2>{}</h2>
-
-            <h2>Here are your task for the day:</h2>
+        <div className="elder-task">
+            <h2></h2>
+            <h3>Task I need done:</h3>
+            {displayTasks()}
         </div>
     );
 }
--- voluntheros-client/src/components/Volunteer.js
+++ voluntheros-client/src/components/Volunteer.js
@@ -1,23 +1,11 @@
-import { Card } from 'react-bootstrap';
 
 function Volunteer({ task }) {
-    console.log(task, 'task?')
-    // .filter(task => task.eldery_id OR task[’eldery_id] === username)
-    function displayTasks() {
-        task.filter(task => {
-            return `
-            <div>${task.title}<div>
-            `
-        })
-    }
-    //if volunteer then gray out card : normal
-    //elderly- check to see if elderly_id matches? display : ignore
-    //id title details volunteers_id elderly_id
+
     return (
         <div>
             <h2></h2>
             <h3>These are my task for the day:</h3>
-            {displayTasks()}
+            {/* {displayTasks()} */}
         </div>
     );
 }
