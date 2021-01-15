b5763eebad2540325ae1157bc6aee78825d6d118
keeks5456 <Akikogreen08@gmail.com>
2021-01-15 11:35:43 -0800

    working on Volunteer

--- voluntheros-client/src/components/Volunteer.js
+++ voluntheros-client/src/components/Volunteer.js
@@ -1,15 +1,13 @@
 import { Card } from 'react-bootstrap';
 
 function Volunteer({ task }) {
+    console.log(task, 'task?')
     // .filter(task => task.eldery_id OR task[’eldery_id] === username)
     function displayTasks() {
-         task.map(task => {
-            console.log(task)
-            return <>
-                <Card style={{ width: '18em' }}>
-                    <Card.Header>{task.title}</Card.Header>
-                </Card>
-            </>
+        task.filter(task => {
+            return `
+            <div>${task.title}<div>
+            `
         })
     }
     //if volunteer then gray out card : normal
@@ -19,7 +17,7 @@ function Volunteer({ task }) {
         <div>
             <h2></h2>
             <h3>These are my task for the day:</h3>
-            {displayTasks}
+            {displayTasks()}
         </div>
     );
 }
