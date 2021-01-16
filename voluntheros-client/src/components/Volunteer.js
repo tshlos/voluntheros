15e88703b58c85e246091fd18dd771ee8dee2317
cortezd334 <cortezd334@gmail.com>
2021-01-15 19:19:00 -0800

    auto refresh

--- voluntheros-client/src/components/Volunteer.js
+++ voluntheros-client/src/components/Volunteer.js
@@ -5,31 +5,8 @@ import { updateTasks} from '../api'
 
 function Volunteer({tasks}) {
 
-    // const [availAmount, setAvailAmount] = useState(0)
-    // const [myAmount, setMyAmount] = useState(0)
-    // const[refresh, setRefresh] = useState(false)
-    // const prevTask = useRef(refresh)
-
-    // console.log(refresh)
-    // console.log(prevTask)
-    
-    //     useEffect(() => {
-    //             if( prevTask.current !== refresh)
-    //             displayAvailTasks()
-    //             displayTakenTasks()
-    //             displayMyTasks()
-    //         }, [refresh]);
-        
-    //     const refresh = useCallback(tasks => {
-    //         if(prevTasks !== tasks)
-    //         displayAvailTasks()
-    //         displayTakenTasks()
-    //         displayMyTasks()
-    //     }, [])
-
     function displayAvailTasks() {
         const availTasks = tasks.filter(task => task.volunteers_id === null)
-        // setAvailAmount(availTasks.length)
         return availTasks.map(task => {
         return <>
             <Card className='grid' key={task.id} style={{ width: '18rem' }}>
@@ -63,7 +40,6 @@ function Volunteer({tasks}) {
 
     function displayMyTasks() {
         const myTasks = tasks.filter(task => task.volunteers_id === 4)
-        // setMyAmount(myTasks.length)
         return myTasks.map(task => {
             return <>
             <Card className='grid' key={task.id} style={{ width: '18rem' }}>
@@ -72,24 +48,21 @@ function Volunteer({tasks}) {
                 Details: {task.details === null ? 'None' : task.details}
                 {/* {task.date} */}
             </Card.Text>
-            <Button>Cancel</Button> 
+            <Button onClick={(e) => removeTask(e, task.id)}>Cancel</Button> 
             </Card>
             </>
         })
     }
 
-//id title details volunteers_id elderly_id
+    function addTask(e, key) {
+        updateTasks({id: key, volunteers_id: 4})
+        window.location.reload();
+    }
 
-function addTask(e, key) {
-    console.log(e)
-    // debugger
-    // e.target.parentElement.remove
-
-    updateTasks({id: key, volunteers_id: 4})
-    .then(console.log)
-    // setRefresh(true)
-}
-
+    function removeTask(e, key) {
+        updateTasks({id: key, volunteers_id: null})
+        window.location.reload();
+    }
 
 
     return (
