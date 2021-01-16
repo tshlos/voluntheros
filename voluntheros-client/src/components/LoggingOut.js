7115684dc4cd3f440edcfab1b484b89d8eee4a1f
keeks5456 <Akikogreen08@gmail.com>
2021-01-15 18:21:58 -0800

    logout pushed to login

--- voluntheros-client/src/components/LoggingOut.js
+++ voluntheros-client/src/components/LoggingOut.js
@@ -1,12 +1,19 @@
+import { useHistory } from 'react-router-dom';
 
 function LoggingOut({logged, setLogged}) {
+
+    const history = useHistory() 
 
     function loggedOut(){
         if(logged === true){
             setLogged(false)
             return <p>Have a good day</p>
+           
         }
+        history.push('/login')
     }
+
+    console.log(logged)
     return (
         <div>
       {loggedOut()}
